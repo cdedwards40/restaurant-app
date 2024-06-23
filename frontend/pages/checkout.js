4905e8b785c05741ef75e3db3824b446fb99a27f
Chris Edwards <cdedwards40@gmail.com>
2024-06-22 22:47:39 -0500

    update env

--- frontend/pages/checkout.js
+++ frontend/pages/checkout.js
@@ -16,7 +16,7 @@ function Checkout() {
   
   // load stripe to inject into elements components
   const stripePromise = loadStripe(
-    "pk_test_51HaLhVGgpfLkdZwmHVQcCOdUzwLWqV7umg9EbicemJqLOcLBPDrPtszruyxf4UzqH0lKwaNj5se3tHldNx92nPjI00Zoi8VgBN"
+    process.env.STRIP_TOKEN
   );
 
   return (
